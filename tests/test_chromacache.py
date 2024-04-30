228e63415821085bbeb8da11755672f07158744c
Imene Kerboua <imenelydia.kr@gmail.com>
2024-04-30 23:17:10 +0200

    update linting

--- tests/test_chromacache.py
+++ tests/test_chromacache.py
@@ -9,6 +9,7 @@ from chromacache import ChromaCache, SentenceTransformerEmbeddingFunction
 
 logging.basicConfig(level=logging.INFO)
 
+
 @pytest.mark.parametrize(
     "model_name",
     [
@@ -17,9 +18,7 @@ logging.basicConfig(level=logging.INFO)
     ],
 )
 def test_encode(model_name: str):
-    embedding_function = SentenceTransformerEmbeddingFunction(
-        model_name=model_name
-    )
+    embedding_function = SentenceTransformerEmbeddingFunction(model_name=model_name)
     chroma_cache = ChromaCache(embedding_function=embedding_function)
     embeddings = chroma_cache.encode("Hello, how are you?")
 
