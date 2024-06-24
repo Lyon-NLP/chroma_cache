dffa2e643305c024dac2f3d036caae7254d82820
Imene Kerboua <imenelydia.kr@gmail.com>
2024-06-24 11:13:19 +0200

    Fix dependencies and tests

---
+++ chromacache/embedding_functions/__init__.py
@@ -0,0 +1,17 @@
+from .AbstractEmbeddingFunction import AbstractEmbeddingFunction
+from .CohereEmbeddingFunction import CohereEmbeddingFunction
+from .LaserEmbeddingFunction import LaserEmbeddingFunction
+from .MistralAIEmbeddingFunction import MistralAIEmbeddingFunction
+from .OpenAIEmbeddingFunction import OpenAIEmbeddingFunction
+from .SentenceTransformerEmbeddingFunction import SentenceTransformerEmbeddingFunction
+from .VoyageAIEmbeddingFunction import VoyageAIEmbeddingFunction
+
+__all__ = [
+    "AbstractEmbeddingFunction",
+    "CohereEmbeddingFunction",
+    "LaserEmbeddingFunction",
+    "MistralAIEmbeddingFunction",
+    "OpenAIEmbeddingFunction",
+    "SentenceTransformerEmbeddingFunction",
+    "VoyageAIEmbeddingFunction",
+]
