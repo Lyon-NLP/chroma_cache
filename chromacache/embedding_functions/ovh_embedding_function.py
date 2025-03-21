b65aa62fcdc267688f8d11579c9a7fbd72e85724
mathieu@wikit.ai <mathieu@wikit.ai>
2025-03-21 09:25:51 +0100

    support dimensions

--- chromacache/embedding_functions/ovh_embedding_function.py
+++ chromacache/embedding_functions/ovh_embedding_function.py
@@ -11,13 +11,14 @@ class OVHAIEmbeddingFunction(LiteLLMEmbeddingFunction):
     def __init__(
         self,
         model_name: str = "multilingual-e5-base",
+        dimensions: int | None = None,
         max_requests_per_minute: int | None = None,
     ) -> None:
         LiteLLMEmbeddingFunction.__init__(
-            self, model_name=model_name, max_requests_per_minute=max_requests_per_minute
-        )
-        self.sleep_time = (
-            60 / max_requests_per_minute if max_requests_per_minute is not None else 0
+            self,
+            model_name=model_name,
+            dimensions=dimensions,
+            max_requests_per_minute=max_requests_per_minute,
         )
         self.endpoint = (
             f"https://{model_name}.endpoints.kepler.ai.cloud.ovh.net/api/batch_text2vec"
@@ -53,7 +54,10 @@ class OVHAIEmbeddingFunction(LiteLLMEmbeddingFunction):
             self.endpoint, data=json.dumps(documents), headers=headers, timeout=30
         )
         if response.status_code == 200:
-            return response.json()
+            embeddings = response.json()
+            if self.dimensions is not None:
+                return [emb[: self.dimensions] for emb in embeddings]
+            return embeddings
         if response.status_code == 404:
             raise RuntimeError(f"Endpoint {self.endpoint} not found.")
         raise RuntimeError("Problem with API")
