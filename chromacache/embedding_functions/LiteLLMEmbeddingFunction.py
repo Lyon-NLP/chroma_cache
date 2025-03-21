0fcdd4dc47a94bd2b3a783959fe360eb60203404
Mathieu Ciancone <73994289+MathieuCiancone@users.noreply.github.com>
2025-03-21 10:01:17 +0100

    14 add embedding function for scaleway (#15)
    * add scaleway emb func
    
    * improve sleep time management
    
    * support dimensions
    
    * increment version
    
    ---------
    
    Co-authored-by: mathieu@wikit.ai <mathieu@wikit.ai>

--- chromacache/embedding_functions/LiteLLMEmbeddingFunction.py
+++ chromacache/embedding_functions/LiteLLMEmbeddingFunction.py
@@ -24,10 +24,11 @@ class LiteLLMEmbeddingFunction(AbstractEmbeddingFunction):
         max_requests_per_minute: int | None = None,
     ) -> None:
         AbstractEmbeddingFunction.__init__(self, model_name=model_name)
+        if dimensions is not None and dimensions < 0:
+            raise ValueError("Argument 'dimension' must be a positive integer.")
         self.dimensions = dimensions
-        self.sleep_time = (
-            60 / max_requests_per_minute if max_requests_per_minute is not None else 0
-        )
+
+        self.max_requests_per_minute = max_requests_per_minute
         self.check_api_key()
 
     @property
@@ -39,6 +40,15 @@ class LiteLLMEmbeddingFunction(AbstractEmbeddingFunction):
     @abstractmethod
     def litellm_provider_prefix(self) -> str:
         """the prefix to use to specify provider in litellm"""
+
+    @property
+    def sleep_time(self) -> float:
+        """time to sleep between two request"""
+        return (
+            60 / self.max_requests_per_minute
+            if self.max_requests_per_minute is not None
+            else 0
+        )
 
     @property
     def collection_name(self) -> str:
